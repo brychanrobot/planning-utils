0badbfb9c613ca5259fef1d18a5e84de81f19313
Bryant Chandler <brychanrobot@gmail.com>
2017-01-20 16:18:35 -0700

    fixed intersection check

--- utils.hpp
+++ utils.hpp
@@ -6,6 +6,11 @@
 #include "geom/Rect.hpp"
 #include "geom/utils.hpp"
 
+inline double randDouble(double bottom, double top) {
+	double randNum = static_cast<double>(rand()) / static_cast<double>(RAND_MAX);
+	return bottom + randNum * (top - bottom);
+}
+
 inline bool hasIntersection(Rect& rect, std::vector<std::shared_ptr<Rect>>& obstacles) {
 	for (const auto& obstacle : obstacles) {
 		if (obstacle->intersects(rect)) {
@@ -45,58 +50,36 @@ inline void generateObstacleHash(std::vector<std::shared_ptr<Rect>>& obstacleRec
 	}
 }
 
-inline bool lineIntersectsObstacles(Coord p1, Coord p2, std::vector<std::vector<bool>>* obstacleHash, int width, int height) {
-	auto dx = p2.x - p1.x;
-	auto dy = p2.y - p1.y;
+// algorithm from http://playtechs.blogspot.com/2007/03/raytracing-on-grid.html
+inline bool lineIntersectsObstacles(int x0, int y0, int x1, int y1, std::vector<std::vector<bool>>* obstacleHash, int width, int height) {
+	int dx = abs(x1 - x0);
+	int dy = abs(y1 - y0);
+	int x = x0;
+	int y = y0;
+	int n = 1 + dx + dy;
+	int x_inc = (x1 > x0) ? 1 : -1;
+	int y_inc = (y1 > y0) ? 1 : -1;
+	int error = dx - dy;
+	dx *= 2;
+	dy *= 2;
 
-	if (p1.x < 0 || p1.y < 0 || p2.x < 0 || p2.y < 0) {
-		return true;
-	}
-
-	/*
-	auto m = 20000.0;  // a big number for vertical slope
-
-	if (abs(dx) > 0.0001) {
-	    m = dy / dx;
-	}
-	*/
-	auto m = clamp(dy / dx, -20000, 20000);
-
-	// printf("m: %f\n", m);
-
-	auto b = -m * p1.x + p1.y;
-
-	if (abs(m) != 20000) {
-		auto minX = std::min(p1.x, p2.x);
-		auto maxX = std::max(p1.x, p2.x);
-
-		for (int ix = minX; ix <= maxX; ix++) {
-			auto y = m * ix + b;
-			// printf("[%.2f, %.2f]:[%.2f, %.2f] %.3f, (%d, %f)\n", p1.x, p1.y, p2.x, p2.y, m, ix, y);
-			// printf("h: [%.2f, %.2f]:[%.2f, %.2f] --------dx: %f m: %f, b: %.2f, (%d, %.2f)\n", p1.x, p1.y, p2.x, p2.y, dx, m, b, ix, y);
-			// printf("%.2d, %.2d\n", (int)y, ix);
-
-			// printf("%.2f, %.2f : %d\n", p1.x, p2.x, ix);
-			if (y > 0 && y < height && (*obstacleHash)[(int)y][ix]) {
-				// printf("returning true\n");
-				return true;
-			}
+	for (; n > 0; --n) {
+		if (x > 0 && x < width && y > 0 && y < height && (*obstacleHash)[y][x]) {
+			return true;
 		}
-	}
 
-	if (abs(m) != 0) {
-		auto minY = std::min(p1.y, p2.y);
-		auto maxY = std::max(p1.y, p2.y);
-
-		for (int iy = minY; iy < maxY; iy++) {
-			auto x = (iy - b) / m;
-			// printf("v: [%.2f, %.2f]:[%.2f, %.2f] --------dx: %f m: %f, b: %.2f, (%.2f, %d)\n", p1.x, p1.y, p2.x, p2.y, dx, m, b, x, iy);
-			if (x > 0 && x < width && (*obstacleHash)[iy][(int)x]) {
-				// printf("returning true\n");
-				return true;
-			}
+		if (error > 0) {
+			x += x_inc;
+			error -= dy;
+		} else {
+			y += y_inc;
+			error += dx;
 		}
 	}
 
 	return false;
+}
+
+inline bool lineIntersectsObstacles(Coord p1, Coord p2, std::vector<std::vector<bool>>* obstacleHash, int width, int height) {
+	return lineIntersectsObstacles(p1.x, p1.y, p2.x, p2.y, obstacleHash, width, height);
 }
